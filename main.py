ba951b717c0adcbd26b522ae0f8f60c43b14cc7e
Solomon Antony <santony@murraystate.edu>
2026-03-04 18:01:54 -0600

    removed duplicate uvicorn.run

--- main.py
+++ main.py
@@ -966,6 +966,6 @@ async def root():
         ]
     }
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+#if __name__ == "__main__":
+##    import uvicorn
+#    uvicorn.run(app, host="0.0.0.0", port=PORT)
